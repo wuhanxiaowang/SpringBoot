622440fd1e4c573c7651f94c89d36491f7e68528
王炎 <2339650998@qq.com>
2019-06-29 17:25:48 +0800

    提交代码

---
+++ src/main/java/com/example/demo/controller/RedisControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.entity.User;
+import com.example.demo.util.RedisUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/demo")
+public class RedisControllerTest {
+
+    @Resource
+    RedisUtil redisUtil;
+    @RequestMapping("/redis")
+
+    public String redisTest() {
+        User user=new User();
+        user.setAge(21);
+        boolean bing = redisUtil.set("Bing", user);
+
+        return String.valueOf(bing);
+    }
+
+
+}
